0208dec2ec4c754c6de3452da8337693bf88d418
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2025-05-17 15:03:20 -0600

    Created Projects
    se genero el proyecto por primera vez

---
+++ Sales-Blazor-Maui/SBM.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SBM.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sales-Blazor-Maui/SBM.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SBM.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
